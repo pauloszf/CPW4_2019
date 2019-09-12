2bbeb3a5263f728ff612590383028fa0430e7239
Paulo Sérgio <paulosergio.zaguini@gmail.com>
2019-09-11 22:36:27 -0400

    Commit inicial

---
+++ trabalho1/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Menu from './components/Menu/Menu';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="inicio">
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
---
+++ trabalho1/src/components/Menu/Menu.js
@@ -0,0 +1,18 @@
+import React from '../../../node_modules/react';
+
+import './Menu.scss';
+
+const Menu = () => 
+<div className="conteiner">
+    <div>
+        <h3>Paulo</h3>
+        <div className="menu">
+            <p>Home</p>
+            <p>Bio</p>
+            <p>Contato</p>
+        </div>
+    </div>
+</div>;
+
+export default Menu;
+
